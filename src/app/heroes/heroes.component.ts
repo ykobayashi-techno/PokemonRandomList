08af14237473bd3a16d29ca023ca682fb8f68614
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-05-30 16:47:38 +0900

    ページをヒーロークラスに対応

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
-  hero = 'Windstorm';
+  constructor() { }
 
   ngOnInit() {
   }
