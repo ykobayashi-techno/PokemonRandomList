81105650234f2a8687f9cf069fbb224ddc489be2
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-05-31 15:52:32 +0900

    getHeroをhttpClientで取得するように

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -23,8 +23,11 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(hero => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
