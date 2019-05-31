e6eced55e7b9ee77157741c5895fa362f66bbbe0
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-05-31 14:57:22 +0900

    最初に/dashboardが表示されるようにデフォルトルートを設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent }
 ];
