75a9123bd6bc97e27230cbf5f21882f1c57bc240
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-05-31 14:12:45 +0900

    HeroServiceを使ってheroesをセットするように変更

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from './../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,21 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
