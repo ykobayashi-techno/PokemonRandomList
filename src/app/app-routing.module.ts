85a103cecc09396b2713081b6c5b48dcebd0b525
ykobayashi-techno <ykobayashi@it-nasu.com>
2019-06-01 10:26:11 +0900

    利用しないコンポーネントの削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: '', redirectTo: '/pokemon', pathMatch: 'full' },
   { path: 'pokemon', component: PokemonListComponent }
 ];
 
